7b8d099a2d58e6c5bdfd807d29ddb7a1a8c16ba4
Misopawa <84553@siswa.unimas.my>
2026-07-24 08:30:40 +0800

    Add GitHub Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import alpinejs from '@astrojs/alpinejs';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [alpinejs()]
+  site: 'https://Misopawa.github.io',
+  base: '/portfolio', 
+  integrations: [tailwind()],
 });
