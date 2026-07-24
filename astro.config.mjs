72564e91a839d7c869a636ea23b51bbd1e5beb12
Misopawa <84553@siswa.unimas.my>
2026-07-24 08:49:27 +0800

    Fix Alpine integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://Misopawa.github.io',
   base: '/portfolio',
+  integrations: [
+    alpinejs(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
